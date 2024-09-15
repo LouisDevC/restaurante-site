f167b579723bb5cd8e3075ee4f006ae4590c79f9
Devlouis_C <DevLouisC@outlook.com>
2024-09-15 18:27:30 -0300

    ajustando mobile
    ajustes relacionados a rolagem não aram obedecidos, devido isso busquei uma melhor solução.

--- assets/code/scroll.js
+++ assets/code/scroll.js
@@ -2,31 +2,20 @@ const allMenuOptions = document.querySelectorAll('.menu li')
 
 allMenuOptions.forEach(option =>{
     option.addEventListener('click', () =>{
-        if(option.textContent === 'nossa história')(
-            window.scroll({
-                top: window.innerHeight,
-                left: 0,
-                behavior: 'smooth'
-        }))
+        if(option.textContent === 'nossa história'){
+            document.querySelector('#history').scrollIntoView({ behavior: 'smooth' })
+        }
         if(option.textContent === 'nossa culinária'){
-            window.scroll({
-                top: window.innerHeight + window.innerHeight,
-                left: 0,
-                behavior: 'smooth'})
+            document.querySelector('#culinary').scrollIntoView({
+            behavior: 'smooth' })
         }
         if(option.textContent === 'Os melhores'){
-            window.scroll({
-                top: window.innerHeight *3,
-                left: 0,
-                behavior: 'smooth'
-            })
+            document.querySelector('#bests').scrollIntoView({
+            behavior: 'smooth' })
         }
         if(option.textContent === 'contato'){
-            window.scroll({
-                top: window.innerHeight *4,
-                left: 0,
-                behavior: 'smooth'
-            })
+            document.querySelector('#contact').scrollIntoView({
+            behavior: 'smooth' })
         }
+        })
     })
-})
